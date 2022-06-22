ed31cac7bcc982a1418db590d8cf367eff562ca9
AmishaGeek <amishageek435@gmail.com>
2022-06-22 13:53:30 +0530

    new

---
+++ header.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Tommy_D\'s_Home_Improvement_Center
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<link rel="preload" href="<?php echo home_url(); ?>/wp-content/themes/tommy-ds-home-improvement-center/assets/fonts/Arial-Black.woff2" as="font" type="font/woff2" crossorigin>
+	<link rel="preload" href="<?php echo home_url(); ?>/wp-content/themes/tommy-ds-home-improvement-center/assets/fonts/Arial-Bold.woff2" as="font" type="font/woff2" crossorigin>
+	<link rel="preload" href="<?php echo home_url(); ?>/wp-content/themes/tommy-ds-home-improvement-center/assets/fonts/Arial-Regular.woff2" as="font" type="font/woff2" crossorigin>
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e('Skip to content', 'tommy-ds-home-improvement-center'); ?></a>
+
+		<header id="masthead" class="site-header">
+			<div class="container">
+				<div class="main-header">
+					<div class="row">
+						<div class="col-lg-2">
+							<div class="site-branding">
+								<?php
+								the_custom_logo();
+								
+								?>
+								
+							</div><!-- .site-branding -->
+						</div>
+						<div class="col-lg-10">
+							<nav id="site-navigation" class="main-navigation">
+								<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e('Primary Menu', 'tommy-ds-home-improvement-center'); ?>
+									<span></span>
+									<span></span>
+									<span></span>
+								</button>
+								<div class="header-menu">
+									<?php
+									wp_nav_menu(
+										array(
+											'theme_location' => 'menu-1',
+											'menu_id'        => 'primary-menu',
+										)
+									);
+									?>
+								</div>
+							</nav><!-- #site-navigation -->
+						</div>
+					</div>
+				</div>
+			</div>
+		</header><!-- #masthead -->
---
+++ template-parts/content-home.php
@@ -0,0 +1,143 @@
+<!-- News Letter SignUp  -->
+<?php #echo do_shortcode('[mc4wp_form id="46"]'); 
+?>
+
+<!-- Start of Banner -->
+<section class="main-banner bg-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/06/main-banner.jpg');">
+    <div class="sec-wp">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="banner-content text-center white-text">
+                        <h3>Your Source for Discount Kitchen Cabinets, Flooring & Moldings</h3>
+                        <h1 class="h1-title">Tommy D's Home Improvement Center</h1>
+                        <div class="banner-btn">
+                            <a href="tel:2157479000" class="sec-btn" title="Call (215) 747-9000">Call <span class="callus"> (215) 747-9000</span></a>
+                            <a href="javascript:void(0);" class="sec-btn outline-btn" title="Contact Us">Contact Us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End of Banner -->
+
+<!-- Start of About Us -->
+<section class="about-us">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <div class="about-img-wp">
+                    <div class="about-img bg-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/06/about-us.jpg');"></div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="about-content">
+                    <h2 class="h2-title">About Us</h2>
+                    <div class="about-text white-text">
+                        <p>
+                            Welcome to Tommy D's, a discount retailer of quality all-wood kitchen cabinets, flooring products, and moldings. All our cabinets are made of solid wood and come in a variety of styles. We can also have a custom cabinet order built and delivered in just a few days! Need a brand new design for your home improvement project? Sit down with our experienced designers to design your dream kitchen!
+                        </p>
+                        <h4>Our Tioga Street location is open Monday to Saturday, from 8:30 AM to 5 PM.</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End of About Us -->
+
+<!-- Start of Service -->
+<div class="main-service-sec">
+    <div class="service-row even">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 order-2 order-lg-1">
+                    <div class="service-content">
+                        <span class="sub-title">Our Services</span>
+                        <h3 class="h3-title">Kitchen Cabinets</h3>
+                        <div class="service-text">
+                            <p>
+                                Renovating a kitchen or building one from the ground up? Choose your kitchen cabinets carefully! When it comes to quality kitchen cabinets at affordable rates, you won't go wrong with Tommy D's.
+                            </p>
+                            <a href="javascript:void(0);" class="learn-more" title="Learn More">Learn More <span class="icon"></span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2">
+                    <div class="service-img-wp">
+                        <div class="service-img bg-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/06/kitchen-kebinets-img.jpg');"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="service-row odd">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="service-img-wp">
+                        <div class="service-img bg-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/06/vanities-img.jpg');"></div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="service-content">
+                        <h3 class="h3-title">Vanities</h3>
+                        <div class="service-text white-text">
+                            <p>
+                                Looking for a new addition to your bathroom decor? Vanity cabinets combine functionality with style – store a full inventory of items while completing the bathroom’s aesthetics. And best of all, they are not as expensive as they used to be.
+                            </p>
+                            <a href="javascript:void(0);" class="learn-more" title="Learn More">Learn More <span class="icon"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="service-row even">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 order-2 order-lg-1">
+                    <div class="service-content">
+                        <h3 class="h3-title">Flooring</h3>
+                        <div class="service-text">
+                            <p>
+                                A flooring solution that’s easy to implement and meets your every requirement? You’ve come to the right place! At Tommy D’s, we offer a wide variety of flooring options, from carpeting to laminate to luxury vinyl.
+                            </p>
+                            <a href="javascript:void(0);" class="learn-more" title="Learn More">Learn More <span class="icon"></span></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2">
+                    <div class="service-img-wp">
+                        <div class="service-img bg-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/06/flooring-img.jpg');"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="service-row odd">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="service-img-wp">
+                        <div class="service-img bg-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/06/molding-img.jpg');"></div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="service-content">
+                        <h3 class="h3-title">Molding</h3>
+                        <div class="service-text white-text">
+                            <p>
+                                Add a finishing touch to your home improvement designs with molding, a process of shaping or trimming surfaces to add depth and dimension to a room. Tommy D's offers a broad assortment of affordable molding designs for residential and commercial properties.
+                            </p>
+                            <a href="javascript:void(0);" class="learn-more" title="Learn More">Learn More <span class="icon"></span></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- End of Service -->
